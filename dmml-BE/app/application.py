7e190d9e4f1fa017d0b04f87c31b342680d3461a
mazkasr <azkasufirman3@gmail.com>
2024-06-25 17:04:23 +0800

    push

---
+++ dmml-BE/app/application.py
@@ -0,0 +1,58 @@
+import joblib
+import pandas as pd
+
+class Application:
+    open: float
+    high: float
+    low: float
+    volume: int
+    year: int
+    month: int
+    day: int
+    day_of_week: int
+    quarter: int
+    sma_20: float
+    sma_50: float
+    sma_100: float
+    return_: float
+    volatility_20: float
+    volatility_50: float
+
+    def __init__(self, open: float, high: float, low: float, volume: int, year: int, month: int, day: int, day_of_week: int, quarter: int, sma_20: float, sma_50: float, sma_100: float, return_: float, volatility_20: float, volatility_50: float):
+        self.open = open
+        self.high = high
+        self.low = low
+        self.volume = volume
+        self.year = year
+        self.month = month
+        self.day = day
+        self.day_of_week = day_of_week
+        self.quarter = quarter
+        self.sma_20 = sma_20
+        self.sma_50 = sma_50
+        self.sma_100 = sma_100
+        self.return_ = return_
+        self.volatility_20 = volatility_20
+        self.volatility_50 = volatility_50
+
+
+    def run(self):
+        # Load the model
+        model_path = 'app/model.pkl'
+        loaded_model = joblib.load(model_path)
+
+        feature_names = ['open', 'high', 'low', 'volume', 'Year', 'Month', 'Day', 'DayOfWeek', 'Quarter',
+                         'SMA_20', 'SMA_50', 'SMA_100', 'Return', 'Volatility_20', 'Volatility_50']
+
+        # Assign Data
+        test_data_dict = {
+            'open': self.open, 'high': self.high, 'low': self.low, 'volume': self.volume,
+            'Year': self.year, 'Month': self.month, 'Day': self.day, 'DayOfWeek': self.day_of_week,
+            'Quarter': self.quarter, 'SMA_20': self.sma_20, 'SMA_50': self.sma_50, 'SMA_100': self.sma_100,
+            'Return': self.return_, 'Volatility_20': self.volatility_20, 'Volatility_50': self.volatility_50
+        }
+
+        # Convert the dictionary to a DataFrame
+        test_data = pd.DataFrame([test_data_dict], columns=feature_names)
+        predictions = loaded_model.predict(test_data)
+        return predictions[0]
---
+++ dmml-BE/app/controllers/prediction_controller.py
@@ -0,0 +1,45 @@
+from app import app
+from flask import request, jsonify
+from app.application import Application
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == "POST":
+        # Parse JSON data from the request
+        data = request.json
+
+        if not data:
+            return jsonify({"error": "No data provided"}), 400
+
+        # Extract specific data fields
+        open_price = data.get('open')
+        high_price = data.get('high')
+        low_price = data.get('low')
+        volume = data.get('volume')
+        year = data.get('year')
+        month = data.get('month')
+        day = data.get('day')
+        day_of_week = data.get('day_of_week')
+        quarter = data.get('quarter')
+        sma_20 = data.get('sma_20')
+        sma_50 = data.get('sma_50')
+        sma_100 = data.get('sma_100')
+        return_rate = data.get('return')
+        volatility_20 = data.get('volatility_20')
+        volatility_50 = data.get('volatility_50')
+
+        # Validate the required fields
+        required_fields = ['open', 'high', 'low', 'volume', 'year', 'month', 'day', 'day_of_week', 'quarter', 'sma_20',
+                           'sma_50', 'sma_100', 'return', 'volatility_20', 'volatility_50']
+        missing_fields = [field for field in required_fields if data.get(field) is None]
+
+        if missing_fields:
+            return jsonify({"error": f"Missing fields: {', '.join(missing_fields)}"}), 400
+
+        # Create an instance of the Application class
+        model = Application(open_price, high_price, low_price, volume, year, month, day, day_of_week, quarter, sma_20, sma_50, sma_100, return_rate, volatility_20, volatility_50)
+
+        output = jsonify({
+            'close': model.run()
+        })
+        return output
